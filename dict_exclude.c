9838a203a80c2c2e489b46e943c53eb3df2ff0f8
Mohan Krishnan <mohangk@gmail.com>
2015-11-29 21:35:49 +0700

    still works

--- dict_exclude.c
+++ dict_exclude.c
@@ -24,9 +24,9 @@ dict_exclude_lexize(PG_FUNCTION_ARGS)
   char     *in = (char *) PG_GETARG_POINTER(1);
   TSLexeme   *res;
   res = palloc0(sizeof(TSLexeme) * 3);
-  /* char *stemmed; */
-  /*  */
-  /* dictionary_load(dictionary_fullpath("data/kata-dasar.txt")); */
+  char *stemmed;
+
+  dictionary_load(dictionary_fullpath("data/kata-dasar.txt"));
   /* stem_singular_word(res[0].lexeme, &stemmed); */
 
   res[0].lexeme = pnstrdup(in, PG_GETARG_INT32(2)) ;
